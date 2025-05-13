335b9978e185d03d2198b55b0a3251edc2b4418c
Wapply <88293580+Wapply@users.noreply.github.com>
2025-05-12 22:11:03 -0300

    002

--- menu.gd
+++ menu.gd
@@ -1,10 +1,10 @@
 extends Node
 
-func _on_start_pressed():
+func _on_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("game.tscn")
 
-func _on_settings_pressed():
+func _on_button_settings_pressed() -> void:
 	get_tree().change_scene_to_file("settings.tscn")
 
-func _on_exit_pressed():
+func _on_button_exit_pressed() -> void:
 	get_tree().quit()
